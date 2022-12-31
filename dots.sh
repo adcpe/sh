2a484f4549ac9fb5e2d3455673ee79d105945993
Andrés Del Carpio <adc@adc.pe>
2022-12-31 16:21:07 -0500

    Changed script

--- dots.sh
+++ dots.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+mkdir -p ~/.config/fish/functions/
 curl https://raw.githubusercontent.com/adcpe/dots/main/.config/fish/functions/redot.fish -o $HOME/.config/fish/functions/redot.fish
