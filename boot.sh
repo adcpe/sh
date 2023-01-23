b07f78105be8a10f803333b542bc1272dacd40d5
Andrés Del Carpio <adc@adc.pe>
2023-01-23 15:47:22 -0500

    Add script to use systemd-boot

---
+++ boot.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+bootctl --path=/boot install
+
+systemctl enable systemd-boot-update
+
+echo "timeout   3" >/boot/loader/loader.conf
+echo "default   @saved" >>/boot/loader/loader.conf
+
+echo "title     Arch Linux with Linux Kernel" >/boot/loader/entries/linux.conf
+echo "linux     /vmlinuz-linux" >>/boot/loader/entries/linux.conf
+echo "initrd    /intel-ucode.img" >>/boot/loader/entries/linux.conf
+echo "initrd    /initramfs-linux.img" >>/boot/loader/entries/linux.conf
+echo "options   root=/dev/sda2 rw" >>/boot/loader/entries/linux.conf
+
+echo "title     Arch Linux with Linux LTS Kernel" >/boot/loader/entries/linux-lts.conf
+echo "linux     /vmlinuz-linux-lts" >>/boot/loader/entries/linux-lts.conf
+echo "initrd    /intel-ucode.img" >>/boot/loader/entries/linux-lts.conf
+echo "initrd    /initramfs-linux-lts.img" >>/boot/loader/entries/linux-lts.conf
+echo "options   root=/dev/sda2 rw" >>/boot/loader/entries/linux-lts.conf
