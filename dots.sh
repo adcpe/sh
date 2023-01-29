1733325281cd2619000c1fd71fff21673c40d12b
Andrés Del Carpio <adc@adc.pe>
2023-01-29 04:49:14 -0500

    Change output path

--- dots.sh
+++ dots.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 mkdir -p ~/.config/fish/functions/
-curl https://raw.githubusercontent.com/adcpe/dots/main/.config/fish/functions/redot.fish -o $HOME/.config/fish/functions/redot.fish
+curl https://raw.githubusercontent.com/adcpe/dots/main/.config/fish/functions/redot.fish -o ~/.config/fish/functions/redot.fish
